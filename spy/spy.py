85d531a7324b87f75c108383634904c496b3fc71
Nicolas Herbaut <nicolas.herbaut@univ-grenoble-alpes.fr>
2018-01-22 15:22:09 +0100

    remove 127.0.0.1 bounded ports

--- spy/spy.py
+++ spy/spy.py
@@ -14,8 +14,8 @@ def get_network_info(iface):
         try:
             mapping[c["Names"][0][1:]] = {"ip": ip,
                                           "private_ip": c["NetworkSettings"]["Networks"]["bridge"]["IPAddress"],
-                                          "ports": [port["PrivatePort"] for port in c["Ports"] if
-                                                    port.get("IP", "0.0.0.0") in ["0.0.0.0", ip]]}
+                                          "ports": [port["PrivatePort"] for port in c["Ports"] if "IP" in port and
+                                                    port.get("IP") in ["0.0.0.0", ip]]}
         except:
             logging.warning("failed to get data for container %s" % c["Names"][0][1:])
 
